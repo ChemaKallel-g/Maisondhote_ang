8eb14146092188d87485d60aced0cf62f4fd33b0
chemakallel-g <chemakallel5@gmail.com>
2025-05-06 00:11:09 +0100

    l-v

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'JoyEvent'`, () => {
+  it(`should have as title 'Maison dhote'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('JoyEvent');
+    expect(app.title).toEqual('Maison dhote');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('JoyEvent app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Maison dhote app is running!');
   });
 });
